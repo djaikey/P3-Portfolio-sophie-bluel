a6aaaecc4ec12b6310d7fc4d3875ad17517207fb
Johanna Keïta <keitajohanna@gmail.com>
2023-04-24 15:12:11 +0200

    querySelector et cerateElement

--- FrontEnd/Index.js
+++ FrontEnd/Index.js
@@ -1,1 +1,20 @@
-// Récupération des elements  gallery
+// Récupération des élements de l'API'
+const elementsfiltres = fetch("http://localhost:5678/api/categories");
+const elementsGallery = fetch("http://localhost:5678/api/works");
+
+//Création des boutons de filtrage
+document.querySelector(".filtres");
+document.createElement("button");
+
+// Affichage des projets de la Class gallery
+const article = elementsGallery[0];
+
+document.querySelector(".gallery");
+
+const ficheElementGallery = document.createElement("article");
+
+const imageElementGallery = document.createElement("img");
+imageElementGallery.src = elementsGallery.imageURL;
+
+const titreElementGallery = document.createElemen("p");
+titreElementGallery.innerText = elementsGallery.title;
