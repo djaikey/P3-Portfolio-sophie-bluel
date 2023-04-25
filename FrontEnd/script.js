9fa05755d91951396b0c93d3f6c2e97d45676544
Johanna Keïta <keitajohanna@gmail.com>
2023-04-25 22:37:55 +0200

    affichage et filtre test 2 frailed

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,68 +1,73 @@
-// Récupération des élements de l'API'
+// fonction affichage par defaut
+function affichageDefault(recuperation) {
+  for (let i = 0; i < recuperation.length; i++) {
+    const elementGallery = recuperation[i];
+
+    document.innerHTML = "";
+    elementGallery = recuperation[i];
+
+    const sectionGallery = document.querySelector(".gallery");
+
+    const sectionFiche = document.createElement("figure");
+    sectionGallery.appendChild(sectionFiche);
+
+    const imageElementFiches = document.createElement("img");
+    return (imageElementFiches.src = elementGallery[i].imageUrl);
+
+    sectionFiches.appendChild(imageElementFiches);
+
+    const titreElementFiches = document.createElement("p");
+    return (titreElementFiches.innerText = elementGallery[i].title);
+
+    sectionFiches.appendChild(titreElementFiches);
+  }
+}
+
+// Création affichage des projets de la Class gallery
+
+// Rattachement aux balises
+
+//Création des boutons de filtrage
+
+const filtrage = document.querySelector(".filtres"); // création div = .classList.add ('filtre')
+
+const fitreTous = document.createElement("button");
+fitreTous.innerText = "Tous";
+
+const filtreObjets = document.createElement("button");
+filtreObjets.innerText = "Objets";
+
+const filtreAppartements = document.createElement("button");
+filtreAppartements.innerText = "Appartements";
+
+const filtreHotelRestaurants = document.createElement("button");
+filtreHotelRestaurants.innerText = "Hôtels & restaurants";
+
+// Rattachement aux balises
+
+filtrage.appendChild(fitreTous);
+filtrage.appendChild(filtreObjets);
+filtrage.appendChild(filtreAppartements);
+filtrage.appendChild(filtreHotelRestaurants);
+
+// Affichage par défaut
+
+// Action de filtrage
+
+//function affichageFiltreTous(recuperation) {
+/*for (let i = 0; i < recuperation.length; i++) {
+    if recuperation [i]) {
+    }
+    const element = recuperation[i];
+  }
+}
+
+// Récupération des élements de l'API' mettre en bas de la page
+
+function RecupererTravaux 
 
 fetch("http://localhost:5678/api/works")
   .then((reponse) => reponse.json())
   .then((recuperation) => {
     console.log(recuperation);
-
-    //Création des boutons de filtrage
-
-    const filtrage = document.querySelector(".filtres");
-
-    const fitreTous = document.createElement("button");
-    fitreTous.innerText = "Tous";
-
-    const filtreObjets = document.createElement("button");
-    filtreObjets.innerText = "Objets";
-
-    const filtreAppartements = document.createElement("button");
-    filtreAppartements.innerText = "Appartements";
-
-    const filtreHotelRestaurants = document.createElement("button");
-    filtreHotelRestaurants.innerText = "Hôtels & restaurants";
-
-    // Rattachement aux balises
-
-    filtrage.appendChild(fitreTous);
-    filtrage.appendChild(filtreObjets);
-    filtrage.appendChild(filtreAppartements);
-    filtrage.appendChild(filtreHotelRestaurants);
-
-    // Création affichage des projets de la Class gallery
-
-    const sectionGallery = document.querySelector(".gallery");
-
-    const sectionFiche = document.createElement("figure");
-
-    const imageElementFiches = document.createElement("img");
-    imageElementFiches.src = recuperation.imageUrl;
-
-    const titreElementFiches = document.createElement("p");
-    titreElementFiches.innerText = recuperation.title;
-
-    // Rattachement aux balises
-
-    sectionGallery.appendChild(sectionFiche);
-    sectionFiches.appendChild(imageElementFiches);
-    sectionFiches.appendChild(titreElementFiches);
-  });
-
-// Affichage par défaut
-
-function premierAffichage(recuperation) {
-  for (let i = 0; i < recuperation.length; i++) {
-    const element = recuperation[i];
-    const imageElementFiches = imageUrl;
-    const titreElementFiches = title;
-  }
-
-  // Action de filtrage
-
-  function affichageFiltreTous(recuperation) {
-    for (let i = 0; i < recuperation.length; i++) {
-      if (condition) {
-      }
-      const element = recuperation[i];
-    }
-  }
-}
+  }); */
