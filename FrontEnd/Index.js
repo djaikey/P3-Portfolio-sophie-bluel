c5aca2a267b3815d2c4e3ff3d6c5d896f899775a
Johanna Keïta <keitajohanna@gmail.com>
2023-04-25 10:57:31 +0200

    gallery et filtre

--- FrontEnd/Index.js
+++ FrontEnd/Index.js
@@ -1,20 +1,28 @@
 // Récupération des élements de l'API'
-const elementsfiltres = fetch("http://localhost:5678/api/categories");
-const elementsGallery = fetch("http://localhost:5678/api/works");
+const elementsfiltres = await fetch("http://localhost:5678/api/categories");
+const elementsGallery = await fetch("http://localhost:5678/api/works");
 
 //Création des boutons de filtrage
-document.querySelector(".filtres");
-document.createElement("button");
+const filtrage = document.querySelector(".filtres");
+const btn1 = document.createElement("button");
+btn1.innerText = "Tous";
+btn2.innerText = "Objets";
+btn3.innerText = "Appartements";
+btn4.innerText = "Hôtels & restaurants";
+const btn2 = document.createElement("button");
+const btn3 = document.createElement("button");
+const btn4 = document.createElement("button");
 
-// Affichage des projets de la Class gallery
+// Création des emplacements d'affichage des projets de la Class gallery
 const article = elementsGallery[0];
 
-document.querySelector(".gallery");
-
-const ficheElementGallery = document.createElement("article");
-
 const imageElementGallery = document.createElement("img");
-imageElementGallery.src = elementsGallery.imageURL;
+imageElementGallery.src = elementsGallery.imageUrl;
 
-const titreElementGallery = document.createElemen("p");
+const titreElementGallery = document.createElement("p");
 titreElementGallery.innerText = elementsGallery.title;
+
+// Rattachement aux balises
+const sectionFiches = document.querySelector(".gallery");
+sectionFiches.appendChild(imageElementGallery);
+sectionFiches.appendChild(titreElementGallery);
