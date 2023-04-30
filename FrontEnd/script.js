be8816bf6bf86ad337d1854007f2366eadc5991e
Johanna Keïta <keitajohanna@gmail.com>
2023-04-30 20:33:30 +0200

    fetch recuperation travaux & category

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,77 +1,27 @@
-// fonction affichage par defaut
-function affichageDefault(recuperation) {
-  for (let i = 0; i < recuperation.length; i++) {
-    const elementGallery = recuperation[i];
+// creation de l'affichage par defaut
 
-    document.innerHTML = "";
-    elementGallery = recuperation[i];
+// Création de l'affiçchage de la galerie par defaut
 
-    const sectionGallery = document.querySelector(".gallery");
+// Récuperation des element de l'API
 
-    const sectionFiche = document.createElement("figure");
-    sectionGallery.appendChild(sectionFiche);
+// Affichage des travaux
 
-    const imageElementFiches = document.createElement("img");
-    return (imageElementFiches.src = elementGallery[i].imageUrl);
-
-    sectionFiches.appendChild(imageElementFiches);
-
-    const titreElementFiches = document.createElement("p");
-    return (titreElementFiches.innerText = elementGallery[i].title);
-
-    sectionFiches.appendChild(titreElementFiches);
-  }
+function recuperationTravaux() {
+  fetch("http://localhost:5678/api/works")
+    .then((reponse) => reponse.json())
+    .then((travaux) => {
+      console.log(travaux);
+    });
 }
+recuperationTravaux();
 
-// Création affichage des projets de la Class gallery
+// Catégorie de filtrage
 
-// Rattachement aux balises
-
-//Création des boutons de filtrage
-
-const filtrage = document.querySelector(".filtres"); // création div = .classList.add ('filtre')
-
-const fitreTous = document.createElement("button");
-fitreTous.innerText = "Tous";
-
-const filtreObjets = document.createElement("button");
-filtreObjets.innerText = "Objets";
-
-const filtreAppartements = document.createElement("button");
-filtreAppartements.innerText = "Appartements";
-
-const filtreHotelRestaurants = document.createElement("button");
-filtreHotelRestaurants.innerText = "Hôtels & restaurants";
-
-// Rattachement aux balises
-
-filtrage.appendChild(fitreTous);
-filtrage.appendChild(filtreObjets);
-filtrage.appendChild(filtreAppartements);
-filtrage.appendChild(filtreHotelRestaurants);
-
-// Affichage par défaut
-
-// Action de filtrage
-
-function affichageFiltreTous(recuperation) {
-  for (let i = 0; i < recuperation.length; i++) {
-    if (condition) {
-    }
-    const element = recuperation[i];
-  }
+function recuperationCategories() {
+  fetch("http://localhost:5678/api/categories")
+    .then((reponse) => reponse.json())
+    .then((categories) => {
+      console.log(categories);
+    });
 }
-
-// Récupération des élements de l'API' mettre en bas de la page
-
-fetch("http://localhost:5678/api/works")
-  .then((reponse) => reponse.json())
-  .then((recuperation) => {
-    console.log(recuperation);
-  });
-
-fetch("http://localhost:5678/api/categories")
-  .then((retour) => retour.json())
-  .then((category) => {
-    console.log(category);
-  });
+recuperationCategories();
