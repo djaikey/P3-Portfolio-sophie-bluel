2fca94381205623bd735cb418375d5b6dba64db5
Johanna Keïta <keitajohanna@gmail.com>
2023-05-03 23:10:19 +0200

    affichage par défaut & filtre tous

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,112 +1,75 @@
-// Affichage par defaut
-
-function affichageParDefaut(travaux) {
-  // Récupération elements du tableau travaux
-
-  for (let i = 0; i < article.length; i++) {
-    const article = travaux[i];
-    const imageUrl = article.imageUrl;
-    const title = article.title;
-    const id = article.id;
-    console.log(id);
-
-    // Choix de l'emplacement parent (balise qui accueuilra les fiches)
-
-    const sectionArticle = document.querySelector(".gallery");
-
-    // Création de l'affiçchage de la galerie par defaut
-
-    const contenuArticle = document.createElement("article");
-
-    imageUrlArticle = document.createElement("img");
-    imageUrlArticle.src = imageUrl;
-
-    titleArticle = document.createElement("p");
-    titleArticle.innerText = title;
-
-    //Rattachement des elements
-
-    sectionArticle.appendChild(contenuArticle);
-    contenuArticle.appendChild(article.imageUrl);
-    contenuArticle.appendChild(article.title);
-  }
-}
-
-// Appel de la fonction affichageParDefdaut()
-
-affichageParDefaut();
-
-//Création des boutons de filtrage
-
-const sectionFiltre = document.querySelector(".filtres");
-
-// Fonction de filtrage
-
-function actionFiltre() {
-
-  // Création filtre "Tous"
-
-  function filreTous(travaux) {
-    
-      const fitrageTous = document.createElement("button");
-      fitrageTous.innerText = "Tous";
-      filtrageTous.id= "btn1"
-
-      filtrageTous.addEventListener(click, () => {
-       
-      filtrageTous.querySelecto(sectionFiltre);
-      sectionArticle.innerHTML="";
-      .affichageParDefaut()
-
-
-     });
-
-  }
-
-  // Appel de la fonction filteTous();
-
-filteTous();
-
-
-  //  Création  filtre "catégory"
-
-  function fitreObjets (categories) {
-
-    // Création du bouton
-
-    const filtrageObjet= document.querySelector(".filtres")
-    filtrageObjet.addEventListener("click", ()=>{
-
-      
-
-    });
-    
-  }
-
-  
-}
-// Appel de la fonction actionFiltre()
-
-actionFiltre()
-
 // Récupération des donnés de l'API
 
-function recuperationTravaux() {
+function affichageParDefaut() {
   fetch("http://localhost:5678/api/works")
     .then((reponse) => reponse.json())
     .then((travaux) => {
       console.log(travaux);
+
+      // Récupération elements du tableau travaux
+
+      for (let i = 0; i < travaux.length; i++) {
+        const article = travaux[i];
+
+        // Choix de l'emplacement parent (balise qui accueuilra les fiches)
+
+        const sectionAffichage = document.querySelector(".gallery");
+
+        // Création de l'affiçchage de la galerie par defaut
+
+        const fiche = document.createElement("div");
+        const image = document.createElement("img");
+        image.src = article.imageUrl;
+        const titre = document.createElement("p");
+        titre.innerText = article.title;
+
+        //Rattachement des elements
+
+        sectionAffichage.appendChild(fiche);
+        fiche.appendChild(image);
+        fiche.appendChild(titre);
+      }
     });
 }
-recuperationTravaux();
+
+// Appel de la fonction d'affichage par defaut
+affichageParDefaut();
 
 // Catégorie de filtrage
 
 function recuperationCategories() {
   fetch("http://localhost:5678/api/categories")
     .then((reponse) => reponse.json())
-    .then((categories) => {
-      console.log(categories);
+    .then((category) => {
+      categoriesFiltres = category;
+      console.log(categoriesFiltres);
+
+      for (let i = 0; i < category.length; i++) {
+        const categories = category[i];
+
+        function boutonTous(categories) {
+          // Choix de l'emplacement parent
+
+          const sectionFiltres = document.querySelector(".filtres");
+
+          // Création du bouton filtre
+
+          const boutonTous = document.createElement("button");
+          boutonTous.innerText = "Tous";
+
+          // Rattachement du bouton
+
+          sectionFiltres.appendChild(boutonTous);
+
+          // Evenement au clique
+
+          boutonTous.addEventListener("click", () => {
+            document.querySelector(".gallery").innerHTML = "";
+            affichageParDefaut();
+          });
+        }
+      }
+      boutonTous();
     });
 }
 recuperationCategories();
