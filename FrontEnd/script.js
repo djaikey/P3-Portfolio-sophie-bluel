13a6ecb81b96b23ead3d82def1ad6d08cbd0982e
Johanna Keïta <keitajohanna@gmail.com>
2023-05-03 12:02:34 +0200

    affichage travaux & category

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,28 +1,95 @@
 // Affichage par defaut
-function affichage(travaux) {
-  for (let i = 0; i < travaux.length; i++) {
-    const element = travaux[i];
-    // Création de l'affiçchage de la galerie par defaut
+
+function affichageParDefaut(travaux) {
+  // Récupération elements du tableau travaux
+
+  for (let i = 0; i < article.length; i++) {
+    const article = travaux[i];
+    const imageUrl = article.imageUrl;
+    const title = article.title;
+    const id = article.id;
+    console.log(id);
+
+    // Choix de l'emplacement parent (balise qui accueuilra les fiches)
 
     const sectionArticle = document.querySelector(".gallery");
 
-    const article = (document.createElement("article").innerHTML += "");
-    article = element;
+    // Création de l'affiçchage de la galerie par defaut
 
-    const imageUrlArticle = document.createElement("img");
-    imageUrlArticle.src = imageUrl.element;
+    const contenuArticle = document.createElement("article");
 
-    const titleArticle = document.createElement("p");
-    titleArticle.innerText = title.element;
+    imageUrlArticle = document.createElement("img");
+    imageUrlArticle.src = imageUrl;
 
-    article.appendChild(sectionArticle);
-    imageUrlArticle.appendChild(article);
-    titleArticle.appendChild(article);
+    titleArticle = document.createElement("p");
+    titleArticle.innerText = title;
+
+    //Rattachement des elements
+
+    sectionArticle.appendChild(contenuArticle);
+    contenuArticle.appendChild(article.imageUrl);
+    contenuArticle.appendChild(article.title);
   }
 }
-affichage();
 
-// Affichage des travaux
+// Appel de la fonction affichageParDefdaut()
+
+affichageParDefaut();
+
+//Création des boutons de filtrage
+
+const sectionFiltre = document.querySelector(".filtres");
+
+// Fonction de filtrage
+
+function actionFiltre() {
+
+  // Création filtre "Tous"
+
+  function filreTous(travaux) {
+    
+      const fitrageTous = document.createElement("button");
+      fitrageTous.innerText = "Tous";
+      filtrageTous.id= "btn1"
+
+      filtrageTous.addEventListener(click, () => {
+       
+      filtrageTous.querySelecto(sectionFiltre);
+      sectionArticle.innerHTML="";
+      .affichageParDefaut()
+
+
+     });
+
+  }
+
+  // Appel de la fonction filteTous();
+
+filteTous();
+
+
+  //  Création  filtre "catégory"
+
+  function fitreObjets (categories) {
+
+    // Création du bouton
+
+    const filtrageObjet= document.querySelector(".filtres")
+    filtrageObjet.addEventListener("click", ()=>{
+
+      
+
+    });
+    
+  }
+
+  
+}
+// Appel de la fonction actionFiltre()
+
+actionFiltre()
+
+// Récupération des donnés de l'API
 
 function recuperationTravaux() {
   fetch("http://localhost:5678/api/works")
