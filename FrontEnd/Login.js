0287772a8702db52dcb6b76c1199b1f37888c14c
Johanna Keïta <keitajohanna@gmail.com>
2023-05-12 19:43:02 +0200

    formulaire de connection

---
+++ FrontEnd/Login.js
@@ -0,0 +1,33 @@
+function formulaireLogin() {
+  const sectionformulaire = document.querySelector(".section-login");
+  //formulaire
+  const formulaire = document.createElement("form");
+  //Label et champs de l'e-mail
+  const labelEmail = document.createElement("p");
+  labelEmail.classList.add("label-formulaire");
+  labelEmail.innerText = "E-mail";
+  const champsEmail = document.createElement("input");
+  champsEmail.classList.add("champs-formulaire");
+  // Label et champs mot de passe
+  const labelMotDePass = document.createElement("p");
+  labelMotDePass.classList.add("label-formulaire");
+  labelMotDePass.innerText = "Mot de pass";
+  const champsMotDePass = document.createElement("input");
+  champsMotDePass.classList.add("champs-formulaire");
+  // bouton d'envoie du formulaire
+  const boutonEnvoieFormulaire = document.createElement("button");
+  boutonEnvoieFormulaire.classList.add("bouton-validation");
+  boutonEnvoieFormulaire.innerText = "Se connecter";
+  // Liens mot de passe oublié
+
+  //Rattachement element formulaire de connection
+
+  sectionformulaire.appendChild(formulaire);
+
+  formulaire.appendChild(labelEmail);
+  formulaire.appendChild(champsEmail);
+  formulaire.appendChild(labelMotDePass);
+  formulaire.appendChild(champsMotDePass);
+  formulaire.appendChild(boutonEnvoieFormulaire);
+}
+formulaireLogin();
