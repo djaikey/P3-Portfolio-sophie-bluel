f94e52e379ead47cf5e30c8ac23b6418904f00bb
Johanna Keïta <keitajohanna@gmail.com>
2023-05-16 20:06:28 +0200

    bouton formulaire addEventListener

--- FrontEnd/Login.js
+++ FrontEnd/Login.js
@@ -1,4 +1,4 @@
-function formulaireLogin(infoLogin) {
+function formulaireLogin() {
   const sectionformulaire = document.querySelector(".section-login");
 
   //formulaire
@@ -6,38 +6,59 @@ function formulaireLogin(infoLogin) {
   formulaire.classList.add("formulaire-login");
 
   //Label et champs de l'e-mail
-  const labelEmail = document.createElement("p");
+  const labelEmail = document.createElement("label");
   labelEmail.classList.add("label-formulaire");
   labelEmail.innerText = "E-mail";
   const champsEmail = document.createElement("input");
   champsEmail.classList.add("champs-formulaire");
 
   // Label et champs mot de passe
-  const labelMotDePass = document.createElement("p");
+  const labelMotDePass = document.createElement("label");
   labelMotDePass.classList.add("label-formulaire");
   labelMotDePass.innerText = "Mot de pass";
   const champsMotDePass = document.createElement("input");
   champsMotDePass.classList.add("champs-formulaire");
   // bouton d'envoie du formulaire
-  const boutonEnvoieFormulaire = document.createElement("button");
-  boutonEnvoieFormulaire.classList.add("bouton-validation");
-  boutonEnvoieFormulaire.innerText = "Se connecter";
-  boutonEnvoieFormulaire.addEventListener("click", (event) => {
-    event.preventDefault;
-    alert("saisie  des informations de connection incorrecte");
+  const boutonEnvoie = document.createElement("button");
+  boutonEnvoie.classList.add("bouton-validation");
+  boutonEnvoie.innerText = "Se connecter";
+  boutonEnvoie.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("click sur Se connecter effectué");
   });
-  // Liens mot de passe oublié
 
   //Rattachement element formulaire de connection
 
   sectionformulaire.appendChild(formulaire);
-  sectionformulaire.appendChild(boutonEnvoieFormulaire);
+
   formulaire.appendChild(labelEmail);
   formulaire.appendChild(champsEmail);
   formulaire.appendChild(labelMotDePass);
   formulaire.appendChild(champsMotDePass);
+  sectionformulaire.appendChild(boutonEnvoie);
 }
-
 formulaireLogin();
 
-function loginRecuperation() {}
+seConnecter();
+/*
+function RecuperationToken() {
+  fetch("http://localhost:5678/api/users/login");
+  (method = "POST"),
+    (headers = {
+      "Content-Type": "application/json",
+      body: JSON.stringify(),
+
+      
+      if(eMail!email || motDePass! password){
+        alert("information erronée")
+      }
+      if(eMail===email || motDePass=== Password{
+        alert("connection en cours")
+      }
+      
+    });
+    
+}
+
+RecuperationToken();
+*/
