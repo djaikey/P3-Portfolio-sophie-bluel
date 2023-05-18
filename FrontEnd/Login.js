a80e929f3a4be2f396349c5ce5795419b79b45c5
Johanna Keïta <keitajohanna@gmail.com>
2023-05-18 19:44:22 +0200

    click event et(recupération infos saisies champs

--- FrontEnd/Login.js
+++ FrontEnd/Login.js
@@ -1,61 +1,85 @@
+//formulaire
 const sectionformulaire = document.querySelector(".section-login");
+const formulaire = document.createElement("form");
+formulaire.classList.add("formulaire-login");
 
-function formulaire() {
-  //formulaire
-  const formulaire = document.createElement("form");
-  formulaire.classList.add("formulaire-login");
+//Rattachement 
 
-  //Label et champs de l'e-mail
-  const labelEmail = document.createElement("label");
-  labelEmail.classList.add("label-formulaire");
-  labelEmail.innerText = "E-mail";
-  const champsEmail = document.createElement("input");
-  champsEmail.classList.add("champs-formulaire");
-  champsEmail.type = "text";
-  champsEmail.name = "email";
+sectionformulaire.appendChild(formulaire);
 
-  console.log(champsEmail.value);
+//Label et champs de l'e-mail
 
-  formulaire.appendChild(labelEmail);
-  formulaire.appendChild(champsEmail);
+const labelEmail = document.createElement("label");
+labelEmail.classList.add("label-formulaire");
+labelEmail.innerText = "E-mail";
+const champsEmail = document.createElement("input");
+champsEmail.classList.add("champs-formulaire");
+champsEmail.type = "text";
+champsEmail.name = "email";
 
-  // Label et champs mot de passe
-  const labelMotDePasse = document.createElement("label");
-  labelMotDePasse.classList.add("label-formulaire");
-  labelMotDePasse.innerText = "Mot de passe";
-  const champsMotDePasse = document.createElement("input");
-  champsMotDePasse.classList.add("champs-formulaire");
-  champsMotDePasse.type = "text";
-  champsMotDePasse.name = "motDePasse";
+//Rattachement
+formulaire.appendChild(labelEmail);
+formulaire.appendChild(champsEmail);
 
-  formulaire.appendChild(labelMotDePasse);
-  formulaire.appendChild(champsMotDePasse);
+// Label et champs mot de passe
 
-  //Rattachement element formulaire de connection
+const labelMotDePasse = document.createElement("label");
+labelMotDePasse.classList.add("label-formulaire");
+labelMotDePasse.innerText = "Mot de passe";
+const champsMotDePasse = document.createElement("input");
+champsMotDePasse.classList.add("champs-formulaire");
+champsMotDePasse.type = "text";
+champsMotDePasse.name = "motDePasse";
 
-  sectionformulaire.appendChild(formulaire);
-}
-formulaire();
+//Rattachement
+
+formulaire.appendChild(labelMotDePasse);
+formulaire.appendChild(champsMotDePasse);
+
+//Lien mot de passe oublié
+
+const lienOublieMDP = document.createElement("a");
+lienOublieMDP.textContent= " Mot de passe oublié"
+lienOublieMDP.classList.add="Lien-oublie"
+
+
+formulaire.appendChild(lienOublieMDP)
+
+//donnée du formulaire
 
 function boutonFormulaire() {
+
   const boutonEnvoie = document.createElement("button");
   boutonEnvoie.classList.add("bouton-validation");
   boutonEnvoie.innerText = "Se connecter";
 
   // Rattachement
   sectionformulaire.appendChild(boutonEnvoie);
-
-  //Evenement
-  boutonEnvoie.addEventListener("click", (evenement) => {
-    evenement.preventDefault();
-    alert("click");
-
-    fetch("http://localhost:5678/api/users/login");
-    {
-      "POST", Headers;
-      {
-      }
+  
+  boutonEnvoie.addEventListener("click", (e) => {
+    e.preventDefault
+    console.log(champsEmail.value);
+    console.log(champsMotDePasse.value);
+    
+    if(champsEmail.value== 0|| champsMotDePasse.value== 0){
+      alert("Erreur dans l’identifiant ou le mot de passe")
     }
+    
+    fetch ("http://localhost:5678/api/users/post",{
+
+      method:"POST",
+      headers: {
+        "accept":"application/json",
+        string:"application/json"  
+      },
+      body:JSON.stringify({   
+        "email": champsEmail.value,
+        "password": champsEmail.value,
+      })
+    });
+
   });
+  
 }
+
 boutonFormulaire();
